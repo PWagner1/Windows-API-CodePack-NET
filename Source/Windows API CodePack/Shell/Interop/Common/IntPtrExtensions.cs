b0f34b6b864b6235785842181ba7e778d30b9b83
Peter Wagner <peterwagner@live.co.uk>
2022-11-11 11:17:14 +0000

    * Check in

---
+++ Source/Windows API CodePack/Shell/Interop/Common/IntPtrExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.WindowsAPICodePack.Shell
+{
+    internal static class IntPtrExtensions
+    {
+        public static T MarshalAs<T>(this IntPtr ptr)
+        {
+            return (T)Marshal.PtrToStructure(ptr, typeof(T));
+        }
+    }
+}
