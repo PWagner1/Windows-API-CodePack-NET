c37d434f95bca9049ffc417a1f0139a5d4293138
Peter Wagner <peterwagner@live.co.uk>
2022-11-11 08:36:03 +0000

    * Shell

---
+++ Source/Windows API CodePack/Shell/Common/ShellSearchCollection.cs
@@ -0,0 +1,14 @@
+﻿//Copyright (c) Microsoft Corporation.  All rights reserved.
+
+namespace Microsoft.WindowsAPICodePack.Shell
+{
+    /// <summary>
+    /// Represents the base class for all search-related classes.
+    /// </summary>
+    public class ShellSearchCollection : ShellContainer
+    {
+        internal ShellSearchCollection() { }
+
+        internal ShellSearchCollection(IShellItem2 shellItem) : base(shellItem) { }
+    }
+}
