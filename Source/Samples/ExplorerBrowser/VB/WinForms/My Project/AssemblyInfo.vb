05bf779bc2f3b9a5d00bfbb633193b0eaec143df
Peter Wagner <peterwagner@live.co.uk>
2024-05-27 10:55:36 +0100

    * Samples

---
+++ Source/Samples/ExplorerBrowser/VB/WinForms/My Project/AssemblyInfo.vb
@@ -0,0 +1,17 @@
+﻿'Copyright (c) Microsoft Corporation.  All rights reserved.
+
+
+Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e4863e9d-cdb6-46f1-b1c9-8d1abd4480b2")>
